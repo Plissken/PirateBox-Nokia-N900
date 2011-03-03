c7e9a4da9ed2b00be33c893ae99846c439d676fd
Robert Plissken <plissken.coding@gmail.com>
2011-03-03 18:31:34 +0100

    updating

---
+++ bin/chatserver.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd $1
+#exec python /home/user/.piratebox/chat/pirateboxchat_start.py 
+exec python pirateboxchat_start.py 
+
+
---
+++ bin/simpleserver.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd $1
+#exec python  /home/user/.piratebox/bin/webserver.py 8001
+exec python  $2/bin/webserver.py 8001
+
+
